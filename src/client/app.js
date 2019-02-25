188692d9be6fd5ef4d2fd2b0eeba0d6550cf9859
Nikolay Tikhonov <luckywastaken@gmail.com>
2019-02-25 16:23:21 +0300

    Installed redux and styled components

--- src/client/app.js
+++ src/client/app.js
@@ -1,9 +1,17 @@
-import React, {Component} from 'react'
-import { hot } from 'react-hot-loader/root'
+import React, {Component} from 'react';
+import { hot } from 'react-hot-loader/root';
+import styled from 'styled-components';
+
+const Header = styled.div`
+    color: rgba(0,0,0,0.8);
+    font-size: 3em;
+    font-family: Arial;
+    text-align: center;
+`
 
 class App extends Component {
     render() {
-        return <div>Welcome to React Boilerplate App</div>
+        return <Header>Welcome to React Boilerplate App</Header>
     }
 }
 
